48f74f2639ba7e20b7db05461444ec29361da4e4
Arthur Chumak <arthurchumak@gmail.com>
2016-11-09 22:26:38 +0300

    custom User model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    father_name = models.CharField(max_length=30)
+
+
+class Deposit(models.Model):
+    pass
+
+
+class Contract(models.Model):
+    pass
+
+
+class Pay(models.Model):
+    pass
