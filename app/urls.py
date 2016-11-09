9d875d6ab01166d3c9c21965bb85d02f7fd1572f
Arthur Chumak <arthurchumak@gmail.com>
2016-11-09 20:46:05 +0300

    deposit views

--- app/urls.py
+++ app/urls.py
@@ -2,12 +2,19 @@ from django.conf.urls import url, include
 
 from app.views import index
 from app.views import login, logout
+import app.views_deposit as deposit
 
 urlpatterns = [
     url(r'^login/', login, name='login'),
     url(r'^logout/', logout, name='logout'),
     url(r'^deposit/', include([
-
+        url(r'^list/', deposit.list, name='list'),
+        url(r'^open/', deposit.open, name='open'),
+        url(r'^refill/', deposit.refill, name='refill'),
+        url(r'^transfer/', deposit.transfer, name='transfer'),
+        url(r'^close/', deposit.close, name='close'),
+        url(r'^extract/', deposit.extract, name='extract'),
+        url(r'^history/', deposit.history, name='history')
     ])),
     url(r'^/*', index, name='index'),
 ]
---
+++ app/views_deposit.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from app.models import Deposit
+
+
+def list(request):
+    return
+
+
+def open(request):
+    return
+
+
+def refill(request):
+    return
+
+
+def transfer(request):
+    return
+
+
+def close(request):
+    return
+
+
+def extract(request):
+    return
+
+
+def history(request):
+    return
