5143de2975bdb642243354f2f48ac31c38501071
Arthur Chumak <arthurchumak@gmail.com>
2016-11-11 12:14:36 +0300

    moved username logic to model

--- app/models.py
+++ app/models.py
@@ -6,6 +6,12 @@ class User(AbstractUser):
     father_name = models.CharField(max_length=30)
     passport_id = models.CharField(max_length=14)
 
+    def get_full_name(self):
+        return self.username if self.is_superuser else "{} {} {}".format(self.last_name, self.first_name, self.father_name)
+
+    def get_short_name(self):
+        return self.username if self.is_superuser else "{} {}. {}.".format(self.last_name, self.first_name[0], self.father_name[0])
+
 
 class Deposit(models.Model):
     percent = models.IntegerField()
