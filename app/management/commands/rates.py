4dc829907105364e3d5d9c78756c39cd9e5e7b5e
Arthur Chumak <arthurchumak@gmail.com>
2016-11-16 12:10:07 +0300

    update rates command

--- app/management/commands/rates.py
+++ app/management/commands/rates.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from app.models import ExchangeRate
 from random import random
+from datetime import date, timedelta
 
 
 class Command(BaseCommand):
@@ -10,11 +11,15 @@ class Command(BaseCommand):
             last_date = ExchangeRate.objects.last().date
         except:
             raise CommandError('No old rates')
-        old_exchange_rates = ExchangeRate.objects.all().filter(date=last_date)
-        for old_exchange_rate in old_exchange_rates:
-            eps = 0.05
-            delta = (random() * eps) + 1 - (eps / 2)
-            exchange_rate = ExchangeRate(from_currency=old_exchange_rate.from_currency,
-                                         to_currency=old_exchange_rate.to_currency,
-                                         index=old_exchange_rate.index * delta)
-            exchange_rate.save()
+        today = date.today()
+        while last_date != today:
+            old_exchange_rates = ExchangeRate.objects.all().filter(date=last_date)
+            last_date += timedelta(days=1)
+            for old_exchange_rate in old_exchange_rates:
+                eps = 0.05
+                delta = (random() * eps) + 1 - (eps / 2)
+                exchange_rate = ExchangeRate(date=last_date,
+                                             from_currency=old_exchange_rate.from_currency,
+                                             to_currency=old_exchange_rate.to_currency,
+                                             index=old_exchange_rate.index * delta)
+                exchange_rate.save()
