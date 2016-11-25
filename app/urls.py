41d6e6d4d03a7268069c0b03debc8ee38e4f2cb4
artemlegonkov <artyom1995a@gmail.com>
2016-11-25 11:47:52 +0300

    # Conflicts: #	app/models.py

--- app/urls.py
+++ app/urls.py
@@ -5,16 +5,12 @@ from app.views import login, logout
 import app.views_deposit as deposit
 import app.views_client as client
 import app.views_admin as admin
+import app.views_contract as contract
 
 urlpatterns = [
     url(r'^login/', login, name='login'),
     url(r'^logout/', logout, name='logout'),
     url(r'^admin/', include([
-        # url(r'^deposit_configurator/(?P<deposit_id>[0-9]+)/', admin.DepositConfigurator, name='depositToArchive'),
-        # url(r'^deposit_configurator/', admin.DepositConfigurator, name='depositConfigurator'),
-        url(r'^newCurrency/', admin.NewCurrency, name='newCurrency'),
-        url(r'^newDeposit/', admin.NewDeposit, name='newDepositFromAdmin'),
-        url(r'^deposits/(?P<deposit_id>[0-9]+)/', admin.ChangeDeposit, name='ChangeDeposit'),
         url(r'^new/', admin.new, name='new'),
         url(r'^list/', admin.list, name='list'),
         url(r'^info/', admin.info, name='info'),
@@ -25,19 +21,26 @@ urlpatterns = [
         url(r'^new/', client.new, name='new'),
         url(r'^info/', client.info, name='info'),
         url(r'^edit/', client.edit, name='edit'),
-        url(r'^allDeposits/(?P<deposit_id>[0-9]+)/', client.newDeposit, name='newDeposit'),
-        url(r'^allDeposits/', client.allDeposits, name='allDeposits'),
-        url(r'^myDeposits/', client.myDeposits, name='allDeposits'),
     ], namespace='client')),
     url(r'^deposit/', include([
-        url(r'^admin/list/(?P<deposit_id>[0-9]+)/', deposit.list, name='listToArch'),
-        url(r'^admin/list/', deposit.list, name='list'),
-        url(r'^open/', deposit.open, name='open'),
+        url(r'^list/(?P<deposit_id>[0-9]+)/', deposit.list, name='listToArch'),
+        url(r'^list/', deposit.list, name='list'),
+        url(r'^new/', deposit.new, name='new'),
+        url(r'^edit/(?P<deposit_id>[0-9]+)/', deposit.edit, name='edit'),
+        url(r'^currency/', deposit.currency, name='currency'),
+
+
         url(r'^refill/', deposit.refill, name='refill'),
         url(r'^transfer/', deposit.transfer, name='transfer'),
         url(r'^close/', deposit.close, name='close'),
         url(r'^extract/', deposit.extract, name='extract'),
         url(r'^history/', deposit.history, name='history')
     ], namespace='deposit')),
+    url(r'^contract/', include([
+        url(r'^all/', contract.all, name='all'),
+        url(r'^new/(?P<deposit_id>[0-9]+)', contract.new, name='new'),
+        url(r'^list/', contract.list, name='list'),
+        url(r'^info/(?P<deposit_id>[0-9]+)', contract.info, name='info'),
+    ], namespace='contract')),
     url(r'', index, name='index'),
 ]
--- app/views_deposit.py
+++ app/views_deposit.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from app.models import Deposit
+from app.forms import *
+
 
 
 def list(request, deposit_id=None):
@@ -8,11 +10,72 @@ def list(request, deposit_id=None):
         d.is_archive = True
         d.save()
     depositList = Deposit.objects.all()
-    return render(request, 'deposit/list_admin.html', {'depositList': depositList})
+    return render(request, 'deposit/list.html', {'depositList': depositList})
 
 
-def open(request):
-    return
+
+def new(request):
+    errors=[]
+    if request.method == 'POST':
+        depositForm = DepositForm(request.POST)
+        if depositForm.is_valid():
+            d=depositForm.save(commit=False)
+            if d.percent<=0:
+                errors.append('Percent must be bigger then 0')
+            elif d.depositType!='Вклад до востребования' and (d.percent_for_early_withdrawal==None or d.percent_for_early_withdrawal<=0):
+                errors.append('percent for early withdrawal must be bigger then 0')
+            elif d.depositType=='Индексируемый вклад' and d.binding_currency==None:
+                errors.append('add binding currency')
+            else:
+                d.save()
+                return redirect('deposit:list')
+    else:
+        depositForm = DepositForm()
+
+    return render(request, 'deposit/new.html', {'depositForm': depositForm, 'errors':errors})
+
+
+
+def edit(request, deposit_id):
+    errors=[]
+    oldDeposit = Deposit.objects.get(pk=deposit_id)
+
+    if request.method == 'POST':
+        depositForm = DepositForm(request.POST)
+        if depositForm.is_valid():
+            d=depositForm.save(commit=False)
+            if d.percent<=0:
+                errors.append('Percent must be bigger then 0')
+            elif d.depositType!='Вклад до востребования' and (d.percent_for_early_withdrawal==None or d.percent_for_early_withdrawal<=0):
+                errors.append('percent for early withdrawal must be bigger then 0')
+            elif d.depositType=='Индексируемый вклад' and d.binding_currency==None:
+                errors.append('add binding currency')
+            else:
+                oldDeposit.is_archive=True;
+                oldDeposit.save()
+                d.save()
+                return redirect('deposit:list')
+    else:
+        depositForm = DepositForm(instance=oldDeposit)
+
+    return render(request, 'deposit/edit.html', {'depositForm': depositForm, 'errors':errors, 'ID':deposit_id})
+
+
+def currency(request):
+    currencyList=Currency.objects.all()
+
+    if request.method == 'POST':
+        form = CurrencyForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = CurrencyForm()
+    else:
+        form = CurrencyForm()
+
+    return render(request, 'deposit/currency.html', {'form': form, 'currencyList':currencyList})
+
+
+
 
 
 def refill(request):
