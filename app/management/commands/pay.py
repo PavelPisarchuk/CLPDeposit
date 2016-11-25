3ebe44972edf30267b5dca3e6e5942e67e87b1ca
Arthur Chumak <arthurchumak@gmail.com>
2016-11-25 23:06:02 +0300

    good pay command

--- app/management/commands/pay.py
+++ app/management/commands/pay.py
@@ -1,4 +1,6 @@
 from django.core.management.base import BaseCommand
+import datetime
+
 from app.models import Bill, Contract, Message, Action
 
 
@@ -8,19 +10,30 @@ class Command(BaseCommand):
         contracts = Contract.objects.all()
         active_contracts = [contract for contract in contracts if contract.is_active()]
         for contract in active_contracts:
-            pay = 500
-            Action.add(
-                action_type_title='PAY',
-                contract=contract,
-                money=pay
-            )
-            contract.bill.push(pay)
+            last_pay = Action.get_last_pay(contract)
+            if last_pay:
+                print('no')
+                last_pay_date = last_pay.datetime
+                if contract.deposit.is_pay_period_month:
+                    d1, d2 = datetime.datetime.now(), last_pay_date
+                    timedelta = (d1.year - d2.year) * 12 + d1.month - d2.month
+                else:
+                    timedelta = (datetime.date.today() - last_pay_date.date()).days
 
-            Message.objects.create(
-                message="Вам выплачено {} по вкладу {}".format(
-                    pay,
-                    contract.deposit.title
-                ),
-                header='Выплата по вкладу',
-                user=contract.bill.client
-            ).save()
+            if not last_pay or timedelta >= contract.deposit.pay_period:
+                pay = 500
+                Action.add(
+                    action_type_title='PAY',
+                    contract=contract,
+                    money=pay
+                )
+                contract.bill.push(pay)
+
+                Message.objects.create(
+                    message="Вам выплачено {} по вкладу {}".format(
+                        pay,
+                        contract.deposit.title
+                    ),
+                    header='Выплата по вкладу',
+                    user=contract.bill.client
+                ).save()
