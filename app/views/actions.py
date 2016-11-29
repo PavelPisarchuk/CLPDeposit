c2b13fe2dd2c996dc69e4d0ee53c8d9f2ee6190f
PavelPisarchuk <5dwolvern@gmail.com>
2016-11-29 05:15:04 +0300

    Небольшой фикс с прошлого коммита, не расскоментил блок Модальное окно на изменение клиента админом, на чтение сообщений(в нём же и удаление), на просмтор операций со счётом

--- app/views/actions.py
+++ app/views/actions.py
@@ -8,13 +8,13 @@ from app.models import Bill
 
 @login_required
 def bill(request):
-    # try:
-    bill = Bill.objects.get(id=request.POST['num'], client=request.user)
-    opers, date, money = [], [], []
-    for i in bill.get_actions().order_by('-id'):
-        opers.append(i.actionType.description)
-        date.append(i.datetime.strftime("%A, %d. %B %Y %I:%M%p"))
-        money.append(i.format_money())
-    return JsonResponse({'info': bill.toString(), 'operations': opers, 'dates': date, 'money': money})
-    # except :
+    try:
+        bill = Bill.objects.get(id=request.POST['num'], client=request.user)
+        opers, date, money = [], [], []
+        for i in bill.get_actions().order_by('-id'):
+            opers.append(i.actionType.description)
+            date.append(i.datetime.strftime("%A, %d. %B %Y %I:%M%p"))
+            money.append(i.format_money())
+        return JsonResponse({'info': bill.toString(), 'operations': opers, 'dates': date, 'money': money})
+    except:
         return redirect('bill:bills')
